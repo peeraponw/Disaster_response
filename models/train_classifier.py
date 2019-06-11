63f92eb9a60b5a310113f783569b00b890c649c5
nongwarmz <nongwarmz@gmail.com>
2019-06-12 00:55:38 +0200

    refactor the prototype to py file

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,68 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet'])
+
+# import libraries
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+import pickle
+from joblib import dump, load
+import warnings; warnings.simplefilter('ignore')
 
 
 def load_data(database_filepath):
-    pass
-
+    '''
+    
+    '''
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('disaster', con=engine)
+    df = df.dropna(how='any')
+    X = df['message']
+    Y = df.drop(columns=['message', 'original', 'genre'])
+    category_names = Y.columns
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    '''
+    
+    '''
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    text = lemmatizer.lemmatize(text.lower())
+    return text
 
 
 def build_model():
-    pass
-
-
+    '''
+    
+    '''
+    pipeline = Pipeline([
+            ('vect', CountVectorizer(tokenizer=tokenize)),
+            ('tfidf', TfidfTransformer()),
+            ('clf', MultiOutputClassifier(RandomForestClassifier())),
+        ])
+    return pipeline
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+    y_pred = model.predict(X_test)
+    for idx in range(y_pred.shape[1]):
+        report = classification_report(Y_test.as_matrix()[:,idx], y_pred[:,idx], output_dict=True)
+        print('Category', category_names[idx])
+        for key in report.keys():
+            print('Label {}:\t Precision={:.3}, Recall={:.3}, F1-score={:.3}'.format(key, report[key]['precision'], report[key]['recall'], report[key]['f1-score']))
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
